3dd74f1b1fb88b87172ca0508d21f1057605071a
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-09-02 11:57:57 -0500

    working on todoList

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -7,17 +8,34 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      name: "John"
+      name: "John",
+      data: [
+        {
+          task: 'Clean Apartment',
+          id: 1,
+          completed: false
+        },
+        {
+          task: 'practice keyboard',
+          id: 2,
+          completed: false
+        }
+      ]
     };
   }
+  
   render() {
     return (
       <div>
          <h1> Hello {this.state.name}</h1>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList task={this.state.data[1].task} />
       </div>
     );
   }
+ 
 }
+
+
 
 export default App;
