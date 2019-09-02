1e8f6dfeba7e7b34992f57282cb0bfcd4f91acd3
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-09-02 15:02:35 -0500

    getting form setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoList from './components/TodoComponents/TodoList';
+import ToDoForm from './components/TodoComponents/TodoForm';
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
@@ -31,7 +32,8 @@ class App extends React.Component {
          <h1> Hello {this.state.name}</h1>
         <h2>Welcome to your Todo App!</h2>
         <TodoList data = {this.state.data} />
-        {console.log(this.state.data)}
+        <ToDoForm />
+        
     
       </div>
     );
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,8 +2,14 @@ import React from "react";
 
 
 const ToDoForm = props => {
+    return (
+        <div>
 
 <input type="text" placeholder="..lets get it done!" onChange={props.InputChange} />
         <button onClick ={props.MoreToDo}>More To Do</button>
         <button onClick ={props.BeGone}>Be Gone!</button>
 }
+</div>
+)
+}
+export default ToDoForm;
